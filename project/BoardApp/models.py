27583527692fe35580e1b6979cb2ca9d2950fd0f
Anna Glazunova <79213278172@yandex.ru>
2022-11-26 17:24:08 +0400

    add

--- project/BoardApp/models.py
+++ project/BoardApp/models.py
@@ -1,5 +1,29 @@
 from django.contrib.auth.models import User
 from django.db import models
+from ckeditor.fields import RichTextField
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    CAT = (
+        ('tanks', 'Танки'),
+        ('healers', 'Хилы'),
+        ('dealer', 'Торговцы'),
+        ('smiths', 'Кузнецы'),
+        ('tanners', 'Кожевники'),
+        ('gild_masters', 'Гилдмастеры'),
+        ('damage_dealers', 'ДД'),
+        ('potion_makers', 'Зельевары'),
+        ('spell_masters', 'Мастера заклинаний')
+    )
+    category = models.CharField(max_length=15, choices=CAT,verbose_name='Категория')
+    dateCreation = models.DateTimeField(auto_now_add= True)
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = RichTextField()
 
 
-
+class Response(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='Текст')
+    status = models.BooleanField(default=False)
+    dateCreation = models.DateTimeField(auto_now_add=True)
